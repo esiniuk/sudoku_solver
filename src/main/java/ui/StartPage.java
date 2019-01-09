99a6ba4c2df86a2c272e54ab0c1ec77f67d3fa70
esiniuk <43136355+esiniuk@users.noreply.github.com>
2019-01-09 16:33:36 +0300

    Update StartPage.java

--- src/main/java/ui/StartPage.java
+++ src/main/java/ui/StartPage.java
@@ -1,16 +1,14 @@
 package ui;
 
-import drivers.DriverFactory;
 import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
 
 import static utils.RunProperties.getWebPortalAddress;
+import static drivers.DriverFactory.driver;
 
 public class StartPage {
-
-    private WebDriver driver = DriverFactory.driver;
-
+    
     public void open() {
 
         driver.get(getWebPortalAddress());
